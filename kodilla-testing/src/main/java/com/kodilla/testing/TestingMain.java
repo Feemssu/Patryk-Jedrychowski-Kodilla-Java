0a3f87e1fde5b894a3315231d7fc0704a9fc5351
Feemssu <feemssu@gmail.com>
2022-11-09 21:05:13 +0100

    6.2 Added test for simpleuser and calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int addTry = calculator.add(2,3);
+        int subtractTry = calculator.subtract(5,1);
+
+        if(addTry == 5) {
+            System.out.println("add test OK");
+        } else {
+            System.out.println("add test Error");
+        }
+
+        if(subtractTry == 4) {
+            System.out.println("subtract test OK");
+        } else {
+            System.out.println("subtract test Error");
+        }
     }
 }
