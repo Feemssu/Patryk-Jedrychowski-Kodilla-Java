7e1e84049e90ab3f975c3f7efc60cfabeabbda8d
Feemssu <feemssu@gmail.com>
2022-11-22 19:19:16 +0100

    7.4 flatMap() and reduce() exercise.

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Continent.java
@@ -0,0 +1,22 @@
+package com.kodilla.stream.world;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Continent {
+
+
+    private final List<Country> countryList = new ArrayList<>();
+
+    public List<Country> getCountryList(){
+        return countryList;
+    }
+    public void addCountry(Country country){
+        countryList.add(country);
+    }
+
+    public boolean removeCountry(Country country){
+        return countryList.remove(country);
+    }
+
+}
